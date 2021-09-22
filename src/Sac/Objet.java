dd62648068d0f5aa39f122393af63bfae7339dcb
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2021-09-22 12:08:39 +0200

    Première version

---
+++ src/Sac/Objet.java
@@ -0,0 +1,14 @@
+package Sac;
+
+public class Objet {
+    private String nom;
+    private float poids;
+    private float valeur;
+
+    public Objet(String n, float p, float v){
+        this.nom = n;
+        this.poids = p;
+        this.valeur = v;
+    }
+
+}
---
+++ src/Sac/SacADos.java
@@ -0,0 +1,26 @@
+package Sac;
+
+import java.util.ArrayList;
+
+public class SacADos {
+    private ArrayList<Objet> Objets = new ArrayList<Objet>();
+    private float poidsMax;
+    private String chemin;
+
+    public SacADos(){
+        ArrayList<Objet> Objets = new ArrayList<Objet>();
+    }
+
+    public SacADos(String c, float poidsmaximal){
+        this.chemin = c;
+        this.poidsMax = poidsmaximal;
+        ArrayList<Objet> Objets = new ArrayList<Objet>();
+    }
+
+    public String toString(){
+
+    }
+
+    public String getObjet(){
+    }
+}
