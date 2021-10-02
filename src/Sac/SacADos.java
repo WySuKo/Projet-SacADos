deb71a82b6eeab7ff513b4ec4017155d12726453
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2021-10-02 11:25:11 +0200

    Update SacADos.java

--- src/Sac/SacADos.java
+++ src/Sac/SacADos.java
@@ -3,19 +3,35 @@ package Sac;
 import java.util.*;
 
 public class SacADos {
-    private ArrayList<Objet> Objets = new ArrayList<Objet>();
+    public ArrayList<Objet> Objets;
     private float poidsMax;
     private String chemin;
 
     public SacADos(){
-         Objets = new ArrayList<Objet>();
+        Objets = new ArrayList<Objet>();
     }
 
     public SacADos(String c, float poidsmaximal){
         this.chemin = c;
         this.poidsMax = poidsmaximal;
-        ArrayList<Objet> Objets = new ArrayList<Objet>();
+        Objets = new ArrayList<Objet>();
     }
 
+    public ArrayList<Objet> getObjets() {
+        return Objets;
+    }
 
+    public float getPoidsMax() {
+        return poidsMax;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("Informations sac à dos :\n");
+        s.append("-Poids maximal : " + this.getPoidsMax() + "\n");
+        for (int i = 0; i<this.Objets.size() ; i++) {
+            s.append(this.getObjets().get(i).toString() +" \n");
+        }
+        return s.toString();
+    }
 }
